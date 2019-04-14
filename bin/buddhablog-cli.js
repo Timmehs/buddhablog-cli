cf4d16ecb00641b14fb8ad17230ff70a045202fd
Tim Sandberg <tasandberg@gmail.com>
2019-04-14 01:03:49 -0700

    new command working

--- bin/buddhablog-cli.js
+++ bin/buddhablog-cli.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 var program = require('commander')
+var path = require('path')
 var VERSION = require('../package').version
 var REPO_URL = 'https://github.com/Timmehs/buddhablog.git'
 var colors = require('colors')
+var clone = require('git-clone')
+var mkdirp = require('mkdirp')
+var exec = require('child_process').exec
 
 program
   .name('buddhablog')
@@ -13,10 +17,36 @@ program
 program
   .command('new [name]')
   .description('Create a new BuddhaBlog project called [name].')
-  .action(function (env, options) {
-    if (env === undefined) {
+  .action(function (dirName, options) {
+    if (dirName === undefined) {
       program.outputHelp()
     }
+
+    mkdirp(dirName, function (err, made) {
+      if (err) {
+        console.log(err)
+      } else {
+        var fullPath = path.join(process.cwd(), dirName)
+        clone(REPO_URL, fullPath, function (err) {
+          if (err) {
+            console.log(
+              colors.red(
+                `Failed to create buddhablog.  Make sure that ${fullPath} is an empty directory.`
+              )
+            )
+            console.log(err)
+            process.exit()
+          } else {
+            exec('rm -rf .git', { cwd: fullPath }, function (err) {
+              if (err) console.log(err)
+            })
+            console.log(colors.green('Success!'))
+            console.log(colors.green(`Next try: `))
+            console.log(`cd ${dirName} && npm install && npm start`)
+          }
+        })
+      }
+    })
 
     console.log(colors.yellow('Cloning BuddhaBlog from master'))
   })
